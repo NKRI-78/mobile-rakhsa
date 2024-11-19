02efdb0211c67a90e4001eddc45f61c6c1b1cd9e
Reihan Agam <reihanagam7@gmail.com>
2024-11-19 17:52:38 +0700

    update

--- lib/features/auth/presentation/pages/login.dart
+++ lib/features/auth/presentation/pages/login.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
@@ -10,8 +9,8 @@ import 'package:rakhsa/common/helpers/snackbar.dart';
 import 'package:rakhsa/common/utils/color_resources.dart';
 import 'package:rakhsa/common/utils/custom_themes.dart';
 import 'package:rakhsa/common/utils/dimensions.dart';
-import 'package:rakhsa/features/auth/presentation/pages/register.dart';
 
+import 'package:rakhsa/features/auth/presentation/pages/register.dart';
 import 'package:rakhsa/features/auth/presentation/provider/login_notifier.dart';
 
 import 'package:rakhsa/shared/basewidgets/button/custom.dart';
