f51b06302558e05a2a63662e9745fd30439b8e43
Reihan Agam <reihanagam7@gmail.com>
2024-11-26 13:19:01 +0700

    update

--- lib/features/news/persentation/pages/list.dart
+++ lib/features/news/persentation/pages/list.dart
@@ -1,8 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+
 import 'package:rakhsa/common/helpers/enum.dart';
 import 'package:rakhsa/common/utils/color_resources.dart';
 import 'package:rakhsa/common/utils/custom_themes.dart';
